540e6c3e22529397303db0575689429a3db9179e
Juan Duran <jugadorduran@gmail.com>
2021-09-14 15:24:23 -0500

    Agregué archivo css al proyecto

--- django_init/django_init/settings.py
+++ django_init/django_init/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS=(os.path.join(BASE_DIR,'django_init/static'))
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'django_init/static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
